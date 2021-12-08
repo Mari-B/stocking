bff5b495108aca3e6429327b056c7ca9e5eddab4
Marina Brunello <marina_brunello@ajg.com>
2021-12-08 17:06:38 +0000

    Install devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: "pk_0d778dd3116c4a7c855b0910b4bf5d50")
+
+    if params[:ticker] == ""
+      @nothing = "Hey! You forgot to enter a symbol"
+    elsif params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      if !@stock
+        @error = "Hey! That stock symbol doesn't exist"
+      end
+    end
   end
+
+  def about
+  end
+
 end
