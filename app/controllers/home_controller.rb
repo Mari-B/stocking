cdbb9c962467853d94c4193aa44ad295929f5eea
Marina Brunello <marina_brunello@ajg.com>
2021-12-14 17:11:16 +0000

    Complete first app draft

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,13 @@ class HomeController < ApplicationController
     @api = StockQuote::Stock.new(api_key: "pk_0d778dd3116c4a7c855b0910b4bf5d50")
 
     if params[:ticker] == ""
-      @nothing = "Hey! You forgot to enter a symbol"
+      @nothing = "Hey! You forgot to enter a symbol."
     elsif params[:ticker]
-      @stock = StockQuote::Stock.quote(params[:ticker])
-      if !@stock
-        @error = "Hey! That stock symbol doesn't exist"
+      @api = StockQuote::Stock.new(api_key: "pk_0d778dd3116c4a7c855b0910b4bf5d50")
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue => exception
+        @error = "Hey! That stock symbol doesn't exist. Try again."
       end
     end
   end
